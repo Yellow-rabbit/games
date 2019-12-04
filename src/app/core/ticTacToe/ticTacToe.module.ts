123006589b29daa655795bed49bc614b6e7596e6
maxypayne <maximlucov@yahoo.com>
2019-12-04 17:59:19 +0100

    started game

--- src/app/core/ticTacToe/ticTacToe.module.ts
+++ src/app/core/ticTacToe/ticTacToe.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TicTacToeComponent } from './ticTacToe.component';
+import { CommonModule } from '@angular/common';
 
 const routes: Routes = [{ path: '', component: TicTacToeComponent }];
 
 @NgModule({
   imports: [
     RouterModule.forChild(routes),
+    CommonModule,
   ],
   declarations: [
     TicTacToeComponent,
